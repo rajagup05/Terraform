d5bc87b6c73ecc09510d270237bbda713a7bd200
Raja gupta <81443339+rajagup05@users.noreply.github.com>
2026-05-24 20:15:22 +0530

    Update main.tf

--- Terraform scripts/VM Instance/main.tf
+++ Terraform scripts/VM Instance/main.tf
@@ -1,1 +1,16 @@
 
+variable "machine_type" {
+    type = map
+    default = {
+        dev = "n2-standard-2"
+        prod = "prod_will_not_work"
+    }
+}
+
+variable "disk_image" {
+    default = "debian-cloud/debian-11"
+}
+
+variable "name_count" { default = [ "server1", "server2", "server3"] }
+
+variable "zone" { default = ["asia-south1-a", "asia-south1-b", "asia-south1-c"] }
